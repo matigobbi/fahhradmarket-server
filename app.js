1afa7f583e7784cb737a3a7188e93b5da840a8be
matigobbi <matigobbi@gmail.com>
2022-05-19 14:32:14 +0200

    ..

--- app.js
+++ app.js
@@ -21,13 +21,12 @@ const auth = require("./routes/auth");
 app.use("/auth", auth);
 
 const path = require('path');
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/dist/build")));
 
 app.use((req, res) => {
   // If no routes match, send them the React HTML.
   res.sendFile(__dirname + "/client/dist/index.html");
 });
-
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
